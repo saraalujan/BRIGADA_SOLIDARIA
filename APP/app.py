d8a995350c78ba0a59dcfe4932514e75a228a172
Debora Gramajo <dgramajo@fi.uba.ar>
2024-11-04 18:05:29 -0300

    cambios

--- APP/app.py
+++ APP/app.py
@@ -2,25 +2,25 @@ from flask import Flask,render_template,url_for,request,redirect
 
 app = Flask(__name__)
 
-@app.route("/about")
-def about():
-    return render_template("abnut.html")
+@app.route("/sobre_nosotros")
+def sobre_nosotros():
+    return render_template("sobre_nosotros.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/nuevos_centros")
+def nuevos_centros():
+    return render_template("nuevos_centros.html")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/service")
-def service():
-    return render_template("service.html")
+@app.route("/estadisticas")
+def estadisticas():
+    return render_template("estadisticas.html")
 
-@app.route("/work")
-def work():
-    return render_template("work.html")
+@app.route("/centros")
+def centros():
+    return render_template("centros.html")
 
 if __name__=="__main__":
     app.run(debug=True,port=8080)
