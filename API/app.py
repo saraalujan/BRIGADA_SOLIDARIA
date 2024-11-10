5ebc94c8a133c03509491d91d519d43be3b8bf18
Debora Gramajo <dgramajo@fi.uba.ar>
2024-11-10 18:29:44 -0300

    avances de la api

--- API/app.py
+++ API/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify,json
 
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-QUERY_TODOS_LOS_CENTROS =
+engine = create_engine("mysql://root@localhost:3306/IDS")
 
-QUERY_
+session = scoped_session(sessionmaker(bind=engine))
+
+QUERY_TODOS_LOS_CENTROS = "SELECT * FROM centros"
+
+app = Flask(__name__)
+
+@app.route("/api/v1/centros", methods=['GET'])
+def get_todos_los_centros():
+    try:
+        return jsonify(centros),200
+    except:
+        pass
+
+if __name__=='__main__':
+    app.run(debug=True)
