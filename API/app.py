ffa4ffea40837b18ef174473c37c315442f86658
Debora Gramajo <dgramajo@fi.uba.ar>
2024-11-11 18:45:06 -0300

    api

--- API/app.py
+++ API/app.py
@@ -14,9 +14,47 @@ def todos_los_centros():
     response = []
 
     for row in result:
-        response.append({'nombre': row[0],'comuna' : row[1] , 'direccion' : row[2] , 'capacidad' : row[3] , 'instalaciones' : row[4] , 'contacto' : row[5]})
+        response.append({'nombre': row[0],
+        'comuna' : row[1] , 
+        'direccion' : row[2] , 
+        'capacidad' : row[3] , 
+        'instalaciones' : row[4] , 
+        'contacto' : row[5]})
 
     return jsonify(response), 200
+
+@app.route('/api/v1/centros',methods=['POST'])
+def anadir_centro():
+    data = request.get_json()
+
+    keys = ('nombre','comuna','direccion','capacidad','direccion','contacto')
+    for key in keys:
+        if key not in data:
+            return jsonify({'error':'Falta el dato {key}'}), 400
+        
+    try:
+        centros.anadir_centro(data), 201
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
+    
+@app.route('/api/v1/centros/buscar', methods=['GET'])
+def buscar_centro():
+    try:
+        result = centros.buscar_centro(request.args)
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
+    response = []
+    for row in result:
+        response.append({'nombre': row[0],
+        'comuna' : row[1] , 
+        'direccion' : row[2] , 
+        'capacidad' : row[3] , 
+        'instalaciones' : row[4] , 
+        'contacto' : row[5]})
+
+    return jsonify(response), 200
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
--- API/centros.py
+++ API/centros.py
@@ -7,6 +7,8 @@ engine = create_engine("mysql://root:1234@localhost:3306/IDS")
 
 QUERY_TODOS_LOS_CENTROS = "SELECT * FROM centros"
 
+QUERY_ANADIR_CENTRO = "INSERT INTO centro (nombre,comuna,direccion,capacidad,direccion,contacto) VALUES (:nombre,:comuna,:direccion,:capacidad,:direccion,:contacto)"
+
 def run_query(query,parameters=None):
     with engine.connect() as conn:
         result = conn.execute(text(query),parameters)
@@ -14,11 +16,48 @@ def run_query(query,parameters=None):
 
     return result
 
+def anadir_centro(data):
+    return run_query(QUERY_ANADIR_CENTRO, data)
+
 def todos_los_centros():
     return run_query(QUERY_TODOS_LOS_CENTROS).fetchall
 
+def por_comuna(comuna):
+    query = QUERY_TODOS_LOS_CENTROS
+
+    filtros.join([f"{key} = '{value}' " for key, value in comuna.items()])
+    filtros = f"WHERE {filtros}" 
+
+    query += filtros
+
+    return run_query(query).fetchall
+
+def por_nombre(nombre):
+    query = QUERY_TODOS_LOS_CENTROS
+
+    filtros.join([f"{key} = '{value}' " for key, value in nombre.items()])
+    filtros = f"WHERE {filtros}" 
+
+    query += filtros
+
+    return run_query(query).fetchall
     
+def por_capacidad(capacidad):
+    query = QUERY_TODOS_LOS_CENTROS
 
+    filtros.join([f"{key} = '{value}' " for key, value in capacidad.items()])
+    filtros = f"WHERE {filtros}" 
 
-if __name__=='__main__':
-    app.run(debug=True)
+    query += filtros
+
+    return run_query(query).fetchall
+
+def por_direccion(direccion):
+    query = QUERY_TODOS_LOS_CENTROS
+
+    filtros.join([f"{key} = '{value}' " for key, value in direccion.items()])
+    filtros = f"WHERE {filtros}" 
+
+    query += filtros
+
+    return run_query(query).fetchall
