e3060d7a3e2638b55ab7343ebd1a02337f1c1d7c
Debora Gramajo <dgramajo@fi.uba.ar>
2024-11-11 14:41:08 -0300

    cambios api

--- API/app.py
+++ API/app.py
@@ -1,22 +1,17 @@
-from flask import Flask, jsonify,json
+from flask import Flask,jsonfy,request
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-engine = create_engine("mysql://root@localhost:3306/IDS")
-
-session = scoped_session(sessionmaker(bind=engine))
-
-QUERY_TODOS_LOS_CENTROS = "SELECT * FROM centros"
+import centros
 
 app = Flask(__name__)
 
-@app.route("/api/v1/centros", methods=['GET'])
-def get_todos_los_centros():
+@app.route('/api/v1/centros',methods=['GET'])
+def todos_los_centros():
     try:
-        return jsonify(centros),200
-    except:
-        pass
+        result = centros.todos_los_centros()
+    except Exception as e:
+        return jsonify({'error':str(e)}),500
 
-if __name__=='__main__':
-    app.run(debug=True)
+    response = []
+
+    for row in result:
+        response.append({'nombre':})
