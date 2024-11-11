57a7756257c78ba52fa296d4131ea8ffaa3d8d7d
Debora Gramajo <dgramajo@fi.uba.ar>
2024-11-11 15:11:34 -0300

    api

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonfy,request
+from flask import Flask, jsonify, request
 
 import centros
 
@@ -9,9 +9,14 @@ def todos_los_centros():
     try:
         result = centros.todos_los_centros()
     except Exception as e:
-        return jsonify({'error':str(e)}),500
+        return jsonify({'error':str(e)}), 500
 
     response = []
 
     for row in result:
-        response.append({'nombre':})
+        response.append({'nombre': row[0],'comuna' : row[1] , 'direccion' : row[2] , 'capacidad' : row[3] , 'instalaciones' : row[4] , 'contacto' : row[5]})
+
+    return jsonify(response), 200
+
+if __name__=='__main__':
+    app.run(debug=True)
